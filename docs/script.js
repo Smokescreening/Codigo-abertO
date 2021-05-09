8ca46e6fdb538c4a1bd9dfebf2140842c4881fbc
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2021-05-09 20:25:19 +0000

    Adding .gitignore file

--- docs/script.js
+++ docs/script.js
@@ -2,4 +2,4 @@ google.charts.load('upcoming', {'packages':['geochart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
-  var data = google
+  var data = google.v
