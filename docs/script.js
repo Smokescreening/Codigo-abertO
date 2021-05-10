90dba17d2876e40b01da6f25573924776abc1797
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2021-05-10 20:50:46 +0000

    adding index.html

--- docs/script.js
+++ docs/script.js
@@ -2,4 +2,4 @@ google.charts.load('upcoming', {'packages':['geochart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
-  var data = google.vi
+  var data = google.vis
