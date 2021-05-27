0c6ba4cf5240469378079f77219f3fd9045dfa19
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2021-05-27 16:05:13 +0000

    Revert "iwlwifi: dvm: don't enable CTS to self"

--- docs/script.js
+++ docs/script.js
@@ -2,4 +2,4 @@ google.charts.load('upcoming', {'packages':['geochart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
-  var data = google.visualization.arrayToData
+  var data = google.visualization.arrayToDataTabl
