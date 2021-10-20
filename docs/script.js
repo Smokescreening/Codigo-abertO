eccf97f833ba673b6f0ab68bec45232ce2ef54ae
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2021-10-20 20:40:54 +0000

    Corrections.

--- docs/script.js
+++ docs/script.js
@@ -7,4 +7,4 @@ function drawRegionsMap() {
   var options = {
     colorAxis: {colors: ['#ACD6FF', '#133D6E']}
   };
-  var chart = new google.visualization.GeoChar
+  var chart = new google.visualization.GeoChart(d
