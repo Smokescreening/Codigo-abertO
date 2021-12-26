77b5f530a261779c7176a5fe9926d72cc64aaa8d
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2021-12-26 11:23:19 +0000

    removing debug output

--- docs/script.js
+++ docs/script.js
@@ -12,4 +12,4 @@ function drawRegionsMap() {
 }
 
 function writeIntoClass(className, message) {
-  document.getElementsByClassName(className)[0].in
+  document.getElementsByClassName(className)[0].inne
