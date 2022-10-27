3210bb36e941473b65f1a444278a6a28152be3f2
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2022-10-27 05:51:14 +0000

    4.5.5

--- lib/member.rb
+++ lib/member.rb
@@ -5,3 +5,6 @@ module DubaiHackathon
   class Member
     attr_reader :user_name, :first_name, :last_name
     attr_reader :title, :skype, :phone
+    attr_reader :email, :time_zone
+
+    def initialize(
