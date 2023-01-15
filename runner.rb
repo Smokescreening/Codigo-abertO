ed50741af64da267ed482c15096d00c69f984510
Smokescreening <114709243+Smokescreening@users.noreply.github.com>
2023-01-15 20:15:54 +0000

    audit: printk USER_AVC messages when audit isn't enabled

--- runner.rb
+++ runner.rb
@@ -15,3 +15,6 @@ module DubaiHackathon
     modified_histogram = DubaiHackathon::adjust_for_canada(hist)
     DubaiHackathon::write_as_yaml file_name: 'histogram.yml', content: modified_histogram
   end
+
+  def self.histogram_hash
+    @histogram_hash ||= YAML::load File.open('histogram.yml', 'r') { |f| f.read }
